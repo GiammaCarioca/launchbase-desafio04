5f4311ded7854431b1df21c8bae0856dc467d37a
Thiago Giammattey <giammattey@gmail.com>
2020-02-14 16:04:54 -0300

    add styled form

--- routes.js
+++ routes.js
@@ -3,7 +3,23 @@ const routes = express.Router()
 const { teachers } = require('./data.json')
 
 routes.get('/', function(req, res) {
-	return res.render('index', { teachers })
+	return res.redirect('/teachers')
+})
+
+routes.get('/teachers', function(req, res) {
+	return res.render('teachers/index', { teachers })
+})
+
+routes.get('/teachers/create', function(req, res) {
+	return res.render('teachers/create')
+})
+
+routes.post('/teachers', function(req, res) {
+	return res.send('recebido!')
+})
+
+routes.get('/students', function(req, res) {
+	return res.send('students')
 })
 
 module.exports = routes
