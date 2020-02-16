de97da2408aca1931b4e10ea1d96822cbb7fe4bf
Thiago Giammattey <giammattey@gmail.com>
2020-02-16 12:07:03 -0300

    add new functionalities: edit & delete teacher

--- controllers/teachers.js
+++ controllers/teachers.js
@@ -4,13 +4,6 @@ const data = require('../data.json')
 const { age, date } = require('../lib/utils')
 
 exports.index = function(req, res) {
-	for (teacher of data.teachers) {
-		teacher.age = age(teacher.birthday)
-		teacher.created_at = new Intl.DateTimeFormat('pt-BR').format(
-			teacher.created_at
-		)
-	}
-
 	return res.render('teachers/index', { teachers: data.teachers })
 }
 
@@ -70,4 +63,48 @@ exports.edit = function(req, res) {
 	}
 
 	return res.render('teachers/edit', { teacher })
+}
+
+exports.put = function(req, res) {
+	const { id } = req.body
+	let index = 0
+
+	const foundTeacher = data.teachers.find(function(teacher, foundIndex) {
+		if (id == teacher.id) {
+			index = foundIndex
+			return true
+		}
+	})
+
+	if (!foundTeacher) return res.send('Teacher not found!')
+
+	const teacher = {
+		...foundTeacher,
+		...req.body,
+		birthday: Date.parse(req.body.birthday)
+	}
+
+	data.teachers[index] = teacher
+
+	fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+		if (err) return res.send('Write file error!')
+
+		return res.redirect(`/teachers/${id}`)
+	})
+}
+
+exports.delete = function(req, res) {
+	const { id } = req.body
+
+	const filteredTeachers = data.teachers.filter(function(teacher) {
+		return teacher.id != id
+	})
+
+	data.teachers = filteredTeachers
+
+	fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+		if (err) return res.send('Write error!')
+
+		return res.redirect('/teachers')
+	})
 }
--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const routes = express.Router()
 const teachers = require('./controllers/teachers')
-const data = require('./data.json')
 
 routes.get('/', function(req, res) {
 	return res.redirect('/teachers')
@@ -18,6 +17,10 @@ routes.get('/teachers/:id', teachers.show)
 routes.get('/teachers/:id/edit', teachers.edit)
 
 routes.post('/teachers', teachers.post)
+
+routes.put('/teachers', teachers.put)
+
+routes.delete('/teachers', teachers.delete)
 
 routes.get('/students', function(req, res) {
 	return res.send('students')
